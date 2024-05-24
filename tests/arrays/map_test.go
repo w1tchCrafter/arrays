1ba74e066fd91809d2d533cf5c97dc94773058e6
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-05-24 17:30:41 -0300

    test (map): adding testing from map

---
+++ tests/arrays/map_test.go
@@ -0,0 +1,20 @@
+package arrays_test
+
+import (
+	"arrays/pkg/arrays"
+	"testing"
+)
+
+func TestMap(t *testing.T){
+  arr := arrays.New(2, 3, 5, 7)
+
+  // should double the given element
+	result := arrays.Map(arr, func(_, n int) int {
+		return n * 2
+	})
+
+	if result[0] != 4 || result[1] != 6 || result[2] != 10 || result[3] != 14 {
+		t.Log(result)
+		t.Fail()
+	}
+}
