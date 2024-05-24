3e4f3bcfcbff2f72d5b3b973eafc37b31229b55e
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-05-24 17:43:08 -0300

    test (unshift): adding a testing from unshift function

---
+++ tests/arrays/unshift_test.go
@@ -0,0 +1,25 @@
+package arrays_test
+
+import(
+  "testing"
+  "arrays/pkg/arrays"
+)
+
+func TestUnshift(t *testing.T) {
+    arr := arrays.New(2, 3, 4, 5, 6)
+    arr.Unshift(1)
+
+    if element, _ := arr.At(0); element != 1 || arr.Len() != 6 {
+        t.Fail()
+    }
+
+    arr.Unshift(-1, 0)
+
+    if element1, _ := arr.At(0); element1 != -1 {
+        t.Fail()
+    }
+    if element2, _ := arr.At(1); element2 != 0 {
+        t.Fail()
+    }
+}
+
