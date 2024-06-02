04f2159dd972c80f1386a902134aba80e469b016
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-06-01 23:27:12 -0300

    refactoring (foreach): follow the MDN docs, and using function (elements, index) instead function (index, elements)

--- pkg/arrays/foreach.go
+++ pkg/arrays/foreach.go
@@ -9,12 +9,13 @@ Example:
 
 names := arrays.New("john", "doe")
 
-	names.ForEach(func(i int, s string) {
+names.ForEach(func(s string, i int) {
 		fmt.Printf("index: %d, value: %s\n", i, s)
 	})
 */
-func (a Array[T]) ForEach(f func(int, T)) {
+
+func (a Array[T]) ForEach(f func(T, int)) {
 	for i, v := range a.elements {
-		f(i, v)
+		f(v, i)
 	}
 }
