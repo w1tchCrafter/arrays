d690a8a5aa554a0df9265630b3bfed08c8faa8fa
w1tchCrafter <guilhermenunes32434@outlook.com>
2024-06-05 01:15:06 -0300

    impl(reduce): stable version of the reduce function

--- pkg/arrays/reduce.go
+++ pkg/arrays/reduce.go
@@ -1,9 +1,42 @@
 package arrays
 
-// unstable due to interface conversion
+import (
+	"reflect"
+)
+
+/*
+# Reduce
+
+The Reduce function runs a function on each array element to produce (reduce it to) a single value.
+
+# Note that this function takes 3 arguments:
+
+  - a Array[T]: the array itself
+  - cb func(accumulator E, item T, index int) E: the callback function to be executed
+  - initial ...E: initial value to be used (optional)
+
+if multiple values are passed to initial only the first one will be used
+
+initial is required if the generic types T and E are different
+
+# This example reduces an array of integers to a single value:
+
+	arr := arrays.New(25, 18, 32, 20, 15)
+
+	max := arrays.Reduce(arr, func(acm, item, _ int) int {
+		return int(math.Max(float64(acm), float64(item)))
+	}) // returns 110
+
+# This example reduces the same array to a single string:
+
+	arr := arrays.New(25, 18, 32, 20, 15)
+
+	str := arrays.Reduce(arr, func(acm string, item, _ int) string {
+		return fmt.Sprint(acm, item)
+	}, "") // returns "2518322015"
+*/
 func Reduce[T, E any](a Array[T], cb func(accumulator E, item T, index int) E, initial ...E) E {
 	var acm E
-	count := 0
 
 	if len(initial) != 0 {
 		acm = initial[0]
@@ -14,13 +47,17 @@ func Reduce[T, E any](a Array[T], cb func(accumulator E, item T, index int) E, i
 			return acm
 		}
 
-		acm = any(first).(E)
-		count = 1
+		acmType := reflect.TypeOf(acm)
+		firstType := reflect.TypeOf(first)
+
+		if acmType != firstType {
+			return acm
+		}
 	}
 
-	for i := count; i < a.Len(); i++ {
+	for i := range a.Len() {
 		current, _ := a.At(i)
-		acm = cb(acm, current, count)
+		acm = cb(acm, current, i)
 	}
 
 	return acm
--- tests/arrays/reduce_test.go
+++ tests/arrays/reduce_test.go
@@ -27,20 +27,27 @@ func TestReduce(t *testing.T) {
 		t.Fail()
 	}
 
-	min := arrays.Reduce(arr, func(acm, item, _ int) int {
-		return int(math.Min(float64(acm), float64(item)))
-	})
+	min := arrays.Reduce(arr, func(acm float64, item, _ int) float64 {
+		return math.Min(float64(acm), float64(item))
+	}, 25)
 
 	if min != 15 {
-		t.Log(min)
 		t.Fail()
 	}
 
 	str := arrays.Reduce(arr, func(acm string, item, _ int) string {
 		return fmt.Sprint(acm, item)
-	})
+	}, "")
 
 	if str != "2518322015" {
+		t.Fail()
+	}
+
+	strtc := arrays.Reduce(arr, func(acm struct{ V int }, item, _ int) struct{ V int } {
+		return struct{ V int }{V: acm.V + item}
+	}, struct{ V int }{V: 0})
+
+	if strtc.V != 110 {
 		t.Fail()
 	}
 }
