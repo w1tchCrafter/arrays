b2804d1a8d1e75f8dac6cc1f7798dd8fe1d102b0
w1tchCrafter <guilhermenunes32434@outlook.com>
2024-06-11 23:46:34 -0300

    updated docs for Every() method

--- pkg/arrays/every.go
+++ pkg/arrays/every.go
@@ -1,5 +1,14 @@
 package arrays
 
+/*
+# Every
+
+The every method checks if all array elements pass a test provided as a callback function.
+
+it returns true if the callback returns true for all of the array elements
+
+in case the callback function does not return true for one item or the array is empty, this method returns false
+*/
 func (a Array[T]) Every(cb func(item T, index int) bool) bool {
 	for i, v := range a.elements {
 		if !cb(v, i) {
